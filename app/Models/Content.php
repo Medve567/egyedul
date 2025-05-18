20862acf2475ebb54648c7c70bd282edd16fdf0c
Hesz Milán <heszmilan.com>
2025-05-18 14:12:46 +0200

    first commit

---
+++ app/Models/Content.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Content extends Model
+{
+    protected $table = 'contents';
+
+    protected $fillable = ['title', 'body', 'img'];
+
+    public $timestamps = false;
+}
